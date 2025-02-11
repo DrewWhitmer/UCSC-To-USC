543d54edd9d8f014833a2fc634e61cb523e39128
Drew Whitmer <aswhitme@ucsc.edu>
2025-02-11 15:22:46 -0500

    wall class + bat spritesheet

--- src/prefabs/wall.js
+++ src/prefabs/wall.js
@@ -1,10 +1,16 @@
 class Wall extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
-
+        scene.add.existing(this).setOrigin(0,0);
+        scene.physics.add.existing(this);
+        this.scene = scene;
     }
 
     update() {
         this.y -= game.settings.speed;
+        if(this.y <= -192) {
+            this.scene.hasWalls = false;
+            this.destroy();
+        }
     }
 }
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -13,12 +13,15 @@ class Play extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, 576, 1152, 'background').setOrigin(.08,0);
 
         //create player
-        this.player = this.physics.add.sprite(game.config.width/2, game.config.height/20, 'faller');
+        this.player = this.physics.add.sprite(game.config.width/2, game.config.height/10, 'faller');
         this.player.setAngularVelocity(1000);
         this.player.body.setCollideWorldBounds(true)
 
         this.moving = false;
-
+        this.speed = game.settings.playerSpeed;
+        this.hasWalls = false;
+        this.createWalls();
+        this.num = 0;
     }
 
     update() {
@@ -36,5 +39,40 @@ class Play extends Phaser.Scene {
         if(!this.moving) {
             this.player.body.setVelocityX(0);
         }
+
+        this.background.tilePositionY += game.settings.speed;
+        if(this.hasWalls) {
+            this.wall1.update();
+            this.wall2.update();
+        }
+    }
+
+    createWalls() {
+        this.num = Phaser.Math.Between(0,2);
+        if(this.num == 0) {
+            this.wall1 = new Wall(this, 0, game.config.height, 'wall');
+            this.wall2 = new Wall(this, game.config.width/3, game.config.height, 'wall');
+        }
+        if(this.num == 1) {
+            this.wall1 = new Wall(this, 0, game.config.height, 'wall');
+            this.wall2 = new Wall(this, 2*game.config.width/3, game.config.height, 'wall');
+        }
+        if(this.num == 2) {
+            this.wall1 = new Wall(this, game.config.width/3, game.config.height, 'wall');
+            this.wall2 = new Wall(this, 2*game.config.width/3, game.config.height, 'wall');
+        }
+        this.physics.add.collider(this.player,this.wall1, () => {
+            this.gameOver();
+        })
+        this.physics.add.collider(this.player,this.wall2, () => {
+            this.gameOver();
+        })
+        this.hasWalls = true;
+        
+    }
+
+    gameOver() {
+        game.settings.playerSpeed = 0;
+        game.settings.speed = 0;
     }
 }
